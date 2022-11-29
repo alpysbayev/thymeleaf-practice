df3e783743cdb802cb48abfd910508c87f2ee469
alpysbayev <adilet.alpysbayev@gmail.com>
2022-11-29 19:53:08 +0600

    first commit

---
+++ src/main/kotlin/com/example/thymeleafpractice/ThymeleafPracticeApplication.kt
@@ -0,0 +1,11 @@
+package com.example.thymeleafpractice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ThymeleafPracticeApplication
+
+fun main(args: Array<String>) {
+	runApplication<ThymeleafPracticeApplication>(*args)
+}
